c2ef3fd08f818cdb886cb06037b9ad67626d10ee
Zhitayr <jiromusikyan@gmail.com>
2021-02-14 15:57:28 +0400

    instal react bootstrap, bootstrap, added component Sign Up

--- estate-website/src/App.js
+++ estate-website/src/App.js
@@ -11,26 +11,75 @@ import LogInForm from "./components/LogInForm";
 import Admin from "./components/Admin";
 import User from "./components/User";
 import AddUser from "./components/AddUser";
+import firebase, { onLog } from "firebase";
+import Signup from "./components/Signup";
+import {Container} from "react-bootstrap";
 
-function App() {
-  return (
-      <>
-      <NavAppBar/>
-      {/*<LogInForm/>*/}
-      <AddUser/>
-      {/*{*/}
-      {/*    <Switch>*/}
-      {/*      <Route path="/admin">*/}
-      {/*        <Admin />*/}
-      {/*      </Route>*/}
-      {/*      <Route path="/user">*/}
-      {/*        <User />*/}
-      {/*      </Route>*/}
-      {/*      <Redirect to="/login" />*/}
-      {/*    </Switch>*/}
-      {/*}*/}
-</>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+    };
+  }
+
+  componentDidMount() {
+    const db = firebase.database();
+    console.log("db");
+    console.log(db);
+  }
+  handleChange = ({ target: { value, id } }) => {
+    this.setState({
+      [id]: value,
+    });
+  };
+
+  createAccount = () => {
+    const { email, password } = this.state;
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((error) => console.log(error));
+  };
+  render() {
+    return (
+      // <div>
+      //   <div className={'login_block'}>
+      //     <input
+      //       type={"text"}
+      //       placeholder={"FirstName"}
+      //       id={"firstName"}
+      //       onChange={this.handleChange}
+      //     />
+      //     <input
+      //       type={"text"}
+      //       placeholder={"LastName"}
+      //       id={"lastName"}
+      //       onChange={this.handleChange}
+      //     />
+      //     <input
+      //       type={"email"}
+      //       placeholder={"Email"}
+      //       id={"email"}
+      //       onChange={this.handleChange}
+      //     />
+      //     <input
+      //       type={"password"}
+      //       placeholder={"Password"}
+      //       id={"password"}
+      //       onChange={this.handleChange}
+      //     />
+      //     <input type={"submit"} onClick={this.createAccount} />
+      //   </div>
+      // </div>
+      <Container className="d-flax align-items-center justify-content-center" style={{minHeight:"100vh"}}>
+        <div className="w-100" style={{maxWidth: "400px"}}>
+          <Signup />
+        </div>
+      </Container>
+    );
+  }
 }
-
-export default App;
--- estate-website/src/index.js
+++ estate-website/src/index.js
@@ -4,12 +4,24 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import firebase from 'firebase';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
+
+const firebaseConfig = {
+    apiKey: "AIzaSyButZ3kdEU1H3iYO0yFMmx8n_2PKNaXwzQ",
+    authDomain: "estate-market-1bbaf.firebaseapp.com",
+    databaseURL: "https://estate-market-1bbaf-default-rtdb.firebaseio.com",
+    projectId: "estate-market-1bbaf",
+    storageBucket: "estate-market-1bbaf.appspot.com",
+    messagingSenderId: "665789247500",
+    appId: "1:665789247500:web:b37a50f9b52afd9d58be29"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
       <App />
-    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
